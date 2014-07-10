0c6402c8fc73adfa34e71238c9d186497f4d120e
ecezhang@ucdavis.edu <victzhang@gmail.com>
2014-07-10 18:39:11 +0000

    Single threaded McPAT RPC server
    McPAT can be run in RPC mode when -socket flag is given.
    In this mode, McPAT will create a UNIX socket and listen.
    McPAT expects a line from the socket with the following format:
    
    input_file_name,print_level,opt_clk,output_file_name
    
    Example:
    /home/user/McPAT/ProcessorDescriptionFiles/Penryn.xml,1,1,/tmp/out.txt

--- main.cc
+++ main.cc
@@ -30,20 +30,52 @@
  ***************************************************************************/
 #include "io.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include "xmlParser.h"
 #include "XML_Parse.h"
 #include "processor.h"
 #include "globalvar.h"
 #include "version.h"
 
+// For Socket Operation
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#define UNIX_PATH_MAX 108
+#define SOCKET_CMD_MAX_LEN 256
+
 
 using namespace std;
 
 void print_usage(char * argv0);
 
+int socket_fd = 0;
+struct sockaddr_un address;
+
+void close_socket()
+{
+	if (socket_fd)
+	{
+		cout << "Closing socket...\n"; 
+		close(socket_fd);
+		unlink(address.sun_path);
+		socket_fd = 0;
+	}
+}
+
+void sigint_handler(int s)
+{
+	close_socket();
+}
+
 int main(int argc,char *argv[])
 {
 	char * fb ;
+	bool use_socket = false;
 	bool infile_specified     = false;
 	int  plevel               = 2;
 	opt_for_clk	=true;
@@ -73,6 +105,12 @@ int main(int argc,char *argv[])
 			i++;
 			opt_for_clk = (bool)atoi(argv[i]);
 		}
+		
+		if (argv[i] == string("-socket"))
+		{
+			infile_specified = true;
+			use_socket = true;
+		}
 	}
 	if (infile_specified == false)
 	{
@@ -82,13 +120,121 @@ int main(int argc,char *argv[])
 
 	cout<<"McPAT (version "<< VER_MAJOR <<"."<< VER_MINOR
 		<< " of " << VER_UPDATE << ") is computing the target processor...\n "<<endl;
-
-	//parse XML-based interface
-	ParseXML *p1= new ParseXML();
-	p1->parse(fb);
-	Processor proc(p1);
-	proc.displayEnergy(2, plevel);
-	delete p1;
+	
+	// Open an Unix domain socket to listen requests
+	if (use_socket)
+	{
+		int connection_fd;
+ 		socklen_t address_length;
+		
+		// Creat an Unix domain socket
+		socket_fd = socket(PF_UNIX, SOCK_STREAM, 0);
+		if(socket_fd < 0)
+		{
+			cerr << "socket() failed\n";
+			return 1;
+		}
+		
+		// generate an uniqe socket name and bind() to it!		
+		memset(&address, 0, sizeof(struct sockaddr_un));
+		address.sun_family = AF_UNIX;
+ 		snprintf(address.sun_path, UNIX_PATH_MAX, "/tmp/mcpat-%d", getpid());
+ 		unlink(address.sun_path);
+ 		cout << "Creating socket at " << address.sun_path << endl;
+ 		if(bind(socket_fd, (struct sockaddr *) &address, sizeof(struct sockaddr_un)) != 0)
+		{
+			cerr << "bind() failed\n";
+			close(socket_fd);
+			return 1;
+		}
+		
+		if(listen(socket_fd, 5) != 0)
+		{
+			cerr << "listen() failed\n";
+			close(socket_fd);
+			unlink(address.sun_path);
+			return 1;
+		}
+		
+		atexit(close_socket);
+		struct sigaction sigIntHandler;
+		sigIntHandler.sa_handler = sigint_handler;
+		sigemptyset(&sigIntHandler.sa_mask);
+		sigIntHandler.sa_flags = 0;
+		sigaction(SIGINT, &sigIntHandler, NULL);
+		
+		while((connection_fd = accept(socket_fd, (struct sockaddr *) &address, &address_length)) > -1)
+		{
+			char buffer[SOCKET_CMD_MAX_LEN];
+			
+			memset(&buffer, 0, SOCKET_CMD_MAX_LEN * sizeof(char));
+			read(connection_fd, buffer, SOCKET_CMD_MAX_LEN);
+			
+			cout << "Received: " << buffer << endl;
+			string if_socket;
+			string of_socket(buffer);
+			ofstream output_file;
+			
+			// first argument: input file name
+			if_socket = of_socket.substr(0, of_socket.find(","));
+			of_socket.erase(0, of_socket.find(",") + 1);
+			// second argument: print_level
+			plevel = atoi(of_socket.substr(0, of_socket.find(",")).c_str());
+			of_socket.erase(0, of_socket.find(",") + 1);
+			// third argument: opt_clk
+			opt_for_clk = (bool)atoi(of_socket.substr(0, of_socket.find(",")).c_str());
+			of_socket.erase(0, of_socket.find(",") + 1);
+			of_socket.erase(of_socket.find("\n") == -1 ? of_socket.length() : of_socket.find("\n"), of_socket.length());
+			// the rest string is output file name
+			cout << "Processing file "  << if_socket << ", output file is " << of_socket
+			 << " (Options: print_level = " << plevel << ", opt_for_clk = " << opt_for_clk << ")" << endl;
+			
+			//parse XML-based interface
+			ParseXML *p1= new ParseXML();
+			fb = new char [if_socket.length()+1];
+			strcpy (fb, if_socket.c_str());
+			p1->parse(fb);
+			Processor proc(p1);
+			
+			// redirect cout and run displayEnergy()
+			cout.flush();
+			ostringstream new_cout;
+			streambuf* orig_cout_rdbuf = cout.rdbuf();
+			cout.rdbuf( new_cout.rdbuf() );
+			proc.displayEnergy(2, plevel);
+			
+			// write this streambuf to file
+			output_file.open(of_socket.c_str(), ofstream::out);
+			if(output_file.is_open())
+			{
+				output_file << new_cout.str();
+				output_file.flush();
+				output_file.close();
+			}
+			else
+			{
+				cerr << "Cannot write output to " << of_socket;
+			}
+			cout.rdbuf( orig_cout_rdbuf );
+			write(connection_fd, "Done.\n", 6);
+			cout << "Done." << endl;
+			
+			delete p1;
+			delete fb;
+			
+			close(connection_fd);
+		}
+		close_socket();
+	}
+	else
+	{
+		//parse XML-based interface
+		ParseXML *p1= new ParseXML();
+		p1->parse(fb);
+		Processor proc(p1);
+		proc.displayEnergy(2, plevel);
+		delete p1;
+	}
 	return 0;
 }
 
